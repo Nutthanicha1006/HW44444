ff69f2ead27cbb6f7a3fe0c11386ea9fc434a4ae
LAPTOP-2QNNL6VT\Lenovo <nutthanicha.kmutt@mail.kmutt.ac.th>
2021-12-25 02:16:40 +0700

    no message

---
+++ HW4Graph.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace HW4Graph
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] ArrayTotalCities;
+            int TotalCities;
+            int NumCities = 1;
+
+            TotalCities = int.Parse(Console.ReadLine());
+            ArrayTotalCities = new string[TotalCities];
+
+            int[,] Graph = new int[ArrayTotalCities.Length, ArrayTotalCities.Length];
+            for (int i = 0; i < TotalCities; i++)
+            {
+                ArrayTotalCities[i] = Console.ReadLine();
+            }
+
+            do
+            {
+                for (int j = 0; j < NumCities; j++)
+                {
+                    Graph[NumCities, j] = int.Parse(Console.ReadLine());
+                }
+                NumCities++;
+            }
+            while (NumCities != TotalCities);
+        }
+    }
+}
